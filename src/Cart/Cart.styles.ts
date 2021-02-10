8721f40e757c743bb1b44397394132b5c0b248ac
AndroAppa <pangana13@gmail.com>
2021-02-10 16:25:35 +0400

    10 feb. 2021

---
+++ src/Cart/Cart.styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.aside `
+
+width: 400px;
+display: flex;
+flex-direction: column;
+justify-content: space-between;
+font-family: Arial, Helvetica, sans-serif;
+padding: 20px;
+
+`
